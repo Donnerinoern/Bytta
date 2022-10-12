d1df1bd8c85f5f7e7c77fb3a725ff56644606168
Donnan <sondresalvestad@gmail.com>
2022-10-12 14:42:14 +0200

    NavBar-completed without logic

--- app/src/main/java/prj/edu/bytta/MainActivity.kt
+++ app/src/main/java/prj/edu/bytta/MainActivity.kt
@@ -3,41 +3,69 @@ package prj.edu.bytta
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import prj.edu.bytta.ui.theme.ByttaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ByttaTheme {
+            MaterialTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Donnan")
-                }
+                /*Surface(
+                    modifier = Modifier.fillMaxSize()
+                    //color = MaterialTheme.colorScheme.background
+                ) {*/
+                //}
+                Content()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+@Preview
+fun Content() {
+    var selectedItem by remember { mutableStateOf(0) }
+    val items = listOf("Home", "Profile", "Messages")
+    val icons = listOf(
+        Icons.Default.Home,
+        Icons.Default.Person,
+        Icons.Default.Email
+        )
+    Scaffold(
+        bottomBar = {
+            BottomAppBar {
+                NavigationBar() {
+                    items.forEachIndexed { index, item ->
+                        NavigationBarItem(
+                            icon = {Icon(imageVector = icons[index], contentDescription = items[index])},
+                            label = {Text(item)},
+                            selected = selectedItem == index,
+                            onClick = { selectedItem = index }
+                        )
+                    }
+                }
+            }
+        }
+    ) {
+        paddingValues -> Greeting(
+            name = "Donnan",
+            modifier = Modifier.padding(paddingValues, )
+        ) {
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ByttaTheme {
-        Greeting("Android")
-    }
+fun Greeting(name: String, modifier: Modifier, function: () -> Unit) {
+    Text(text = "Hello $name!")
 }
