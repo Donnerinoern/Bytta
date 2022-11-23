225ca022eb1a8624144c9e94c85e1781d7c32152
rosla <roslanderik@gmail.com>
2022-11-23 22:26:17 +0100

    Chatrooms med ekstra features

--- app/src/main/java/prj/edu/bytta/Extra.kt
+++ app/src/main/java/prj/edu/bytta/Extra.kt
@@ -1,17 +1,32 @@
 package prj.edu.bytta
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import android.content.Intent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import androidx.compose.material.Text
+
 
 @Composable
 fun SingleMessage(message: String, isCurrentUser: Boolean) {
@@ -37,39 +52,184 @@ fun SingleMessage(message: String, isCurrentUser: Boolean) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TextFormField(value: String, onValueChange: (String) -> Unit, label: String, keyboardType: KeyboardType, visualTransformation: VisualTransformation) {
-    OutlinedTextField(
-        value = value, onValueChange = onValueChange,
-        label = {
-            Text(
-                label
+fun UserCard(trade: Trade,) {
+    val context = LocalContext.current
+    Spacer(modifier = Modifier.height(5.dp))
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        onClick = {/*TODO*/
+                 val intent = Intent(context, MessageActivity::class.java)
+                  context.startActivity(intent)},
+        shape = RoundedCornerShape(20.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        )
+    ) {
+        Row(modifier = Modifier.padding(all = 5.dp)) {
+            Image(
+                painter = painterResource(R.drawable.petter_northug_2018_scanpix),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    // Set image size to 40 dp
+                    .size(40.dp)
+                    // Clip image to be shaped as a circle
+                    .clip(CircleShape)
             )
-        },
-        maxLines = 1,
-        modifier = Modifier
-            .padding(horizontal = 20.dp, vertical = 5.dp)
-            .fillMaxWidth(),
-        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
-        singleLine = true,
-        visualTransformation = visualTransformation
-    )
+            Spacer(modifier = Modifier.width(5.dp))
+            Column {
+                Text(text = trade.user)
+                // Add a vertical space between the author and message texts
+                Spacer(modifier = Modifier.height(5.dp))
+                Text(text = trade.item)
+                Spacer(modifier = Modifier.height(5.dp))
+
+            }
+        }
+
+       }
 }
 
 @Composable
-fun Knapp(title: String, onClick: () -> Unit, backgroundColor: Color) {
-    Button(
-        onClick = onClick,
-        colors = ButtonDefaults.buttonColors(
-            containerColor = backgroundColor,
-            contentColor = Color.White
-        ),
-        modifier = Modifier.fillMaxWidth(),
+fun TopBarDetails(
+    selectedUserDetails: User,
+    back: () -> Unit
+) {
+    val focusManager = LocalFocusManager.current
+
+    Card(
+
         shape = RoundedCornerShape(0),
-
     ) {
-        Text(
-            text = title
+        Box(
+            contentAlignment = Alignment.BottomCenter,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(65.dp)
         )
+        {
+            AsyncImage(
+                model = ImageRequest.Builder(
+                    LocalContext.current).data(selectedUserDetails.profilePic),
 
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentScale = ContentScale.Crop,
+                )
+
+
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    BackButton(
+                        back = {
+                            back()
+                            focusManager.clearFocus()
+                        },
+                        icon = Icons.Default.ArrowBack,
+                    )
+                }
+            }
+        }
     }
 }
+
+@Composable
+fun BackButton(back: () -> Unit, icon: ImageVector, ) {
+    Column(
+        modifier = Modifier
+            .padding(start = 15.dp)
+    ) {
+
+        Box(modifier = Modifier
+            .size(35.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(Color.White)
+            .border(width = 2.dp, color = Color.White, shape = RoundedCornerShape(10.dp))
+        ) {
+            IconButton(onClick ={
+                back()
+            }
+            ) {
+                Icon(
+                    icon,
+                    contentDescription = null,
+                    modifier = Modifier.size(25.dp),
+                    tint = Color.Black
+                )
+            }
+
+
+        }
+    }
+}
+
+@Composable
+fun SingelMessage(message: MessageDetails) {
+    Row(
+        horizontalArrangement = if(message.messageByCurrentUser)
+            Arrangement.End else Arrangement.Start,
+        modifier = Modifier
+            .fillMaxWidth(
+                if (message.messageByCurrentUser) 1f
+                else 0.5f
+            )
+            .padding(bottom = 10.dp)
+    ) {
+        if (message.messageByCurrentUser)
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = if (message.messageByCurrentUser) MaterialTheme.colorScheme.primary else Color.White,
+
+            ),
+                shape = RoundedCornerShape(30),
+            ) {
+                Text(
+                    text = message.message,
+                    textAlign = if (message.messageByCurrentUser) TextAlign.End
+                    else
+                        TextAlign.Start,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(15.dp),
+                    color = if (!message.messageByCurrentUser) MaterialTheme.colorScheme.primary else Color.White)
+
+            }
+    }
+}
+
+@Composable
+fun Boxern(
+    size: Int? = null,
+    height: Int? = null,
+    width: Int? = null,
+) {
+    val fix = size != null || height != null || width != null
+    
+    if (fix) {
+        if (size != null) {
+            Spacer(modifier = Modifier.size(size.dp))
+        } else if (height != null && width != null){
+            Spacer(modifier = Modifier
+                .height(height.dp)
+                .width(width.dp)
+            )
+        } else if(height != null){
+            Spacer(modifier = Modifier.height(height.dp))
+        } else if (width != null){
+            Spacer(modifier = Modifier.width(width.dp))
+        }else {
+            Spacer(modifier = Modifier.fillMaxSize(1f))
+        }
+    }
+}
+
+
